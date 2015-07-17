e291b68eb74e48a1c818ac6f135989878335e359
Ozan Tunca <ozantunca@Ozans-MacBook-Air.local>
2015-07-18 02:50:51 +0300

    Unlimited column support, --sortBy option

--- index.js
+++ index.js
@@ -1,69 +1,97 @@
 #! /usr/bin/env node
 
-var fs = require('fs');
-var _ = require('underscore');
-var argv = require('optimist').argv;
-var async = require('async');
-var glob = require('glob');
-var colors = require('colors');
-var OUTPUT_LIMIT = 10;
+var fs = require('fs')
+  , _ = require('underscore')
+  , argv = require('optimist').argv
+  , async = require('async')
+  , glob = require('glob')
+  , colors = require('colors/safe')
+  , usefulColors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan']
+  , OUTPUT_LIMIT = 10
+  , files = argv._;
 
-var files = argv._;
 
-if(files == 0) {
+if (files == 0) {
   throw new Error('No argument or file specified');
 }
 
-if(files.length == 1) {
+// Assign default columns
+argv.cols = [ 'count', 'requested_resource' ];
+argv.prefixes = [];
+argv.sortBy = argv.sortBy || argv.s;
+
+// Parse prefixes and column choices
+_.each(argv, function (arg, key) {
+  var match = key.match(/^p(refix){0,1}([0-9]+)$/);
+
+  if (match) {
+    argv.prefixes[match[2] - 1] = arg;
+    return;
+  }
+
+  match = key.match(/^c(ol){0,1}([0-9]+)$/);
+
+  if (match)
+    argv.cols[match[2] - 1] = arg;
+});
+
+// If files array consists of only one value it could
+// be either a single file or a directory of files
+// to be processed.
+if (files.length == 1) {
   async.auto({
-    // check directory
+    // Check if the file is a directory
     directory: function (next) {
       glob(files[0] + '/*', next);
     },
 
-    // if not directory, get single file
+    // If it's not directory, pass single file
     singleFile: ['directory', function (next, results) {
-      if(results.directory && !!results.directory.length) return next(null, results.directory);
+      if (results.directory && !!results.directory.length) return next(null, results.directory);
       glob(files[0], next);
     }]
   }, function (err, results) {
-    if(err) throw err;
-    if(!results.singleFile.length) throw new Error('No file found.');
+    if (err) throw err;
+    if (!results.singleFile.length) throw new Error('No file found.');
+
+    // assign files
     files = results.singleFile;
 
-    // loop through files
+    // Loop through files
     async.map(files, function (file, next) {
-      // read file contents
+
+      // Read file contents
       fs.readFile(file, 'utf-8', function (err, data) {
-        if(err) return next(err);
+        if (err) return next(err);
         next(null, data.split('\n'));
       });
     }, processLogs);
   });
 }
 else {
+  // Loop through files
   async.map(files, function (file, next) {
-    // read file contents
+
+    // Read file contents
     fs.readFile(file, 'utf-8', function (err, data) {
-      if(err) return next(err);
+      if (err) return next(err);
       next(null, data.split('\n'));
     });
   }, processLogs);
 }
 
 
+// Processing logs
 function processLogs(err, logs) {
-  if(err) throw err;
-
-  var col1 = argv.col1 || 'count'
-    , col2 = argv.col2 || 'requested_resource';
+  if (err) throw err;
 
   // Split and parse lines
   logs = _.chain(logs)
   .reduce(function (allData, fileData) {
     return allData.concat(fileData);
   })
-  .compact().map(function (line) {
+  .compact()
+  .map(function (line) { // Parse log lines for easy access
     var attributes = line.split(' ');
     return log = {
       'timestamp': attributes[0],
@@ -83,44 +111,83 @@ function processLogs(err, logs) {
     };
   }).value();
 
+  var countIndex = argv.cols.indexOf('count');
+
+  // Return count
+  if (countIndex > -1) {
+    var tempCols = argv.cols.slice(0);
+
+    tempCols.splice(countIndex, 1);
+
+    var logs = _.chain(logs)
+    .countBy(function (l) {
+      return JSON.stringify(_.map(tempCols, function (c) { return l[c]; }));
+    })
+    .pairs()
+    .map(function (l) {
+      var count = l[1];
+      l = JSON.parse(l[0]);
+      l.splice(countIndex, 0, count);
+      return l;
+    });
+
+    if (argv.prefixes.length)
+      logs = logs.filter(filterLogs)
+
+    logs = logs
+    .sortBy(argv.sortBy ? argv.sortBy - 1 : 0)
+    .value();
+  }
+  // Return custom column2
+  else {
+    var tempCols = argv.cols.slice(0);
+    var logs = _.chain(logs)
+    .map(function (log) {
+      return _.values(_.pick.apply(this, [log].concat(tempCols)));
+    });
+
+    if (argv.prefixes.length)
+      logs = logs.filter(filterLogs)
+
+    logs = logs
+    .sortBy(argv.sortBy ? argv.sortBy - 1 : 0)
+    .value();
+  }
+
+  // Overwrite output limit according to --limit argument
   OUTPUT_LIMIT = argv.limit || OUTPUT_LIMIT;
   OUTPUT_LIMIT = OUTPUT_LIMIT <= logs.length ? OUTPUT_LIMIT : logs.length;
 
-  // return count
-  if(col1 == 'count') {
-    var logs = _.chain(logs)
-    .countBy(col2)
-    .pairs()
-    .sortBy(1)
-    .value();
-  }
-  // return custom column2
-  else {
-    var logs = _.chain(logs)
-    .sortBy(col1)
-    .map(function (log) {
-      return [ log[col2], log[col1] ];
-    })
-    .value();
-  }
-
-  if(argv.prefix1 || argv.prefix2) {
-    logs = logs.filter(function (log) {
-      return (argv.prefix1 ? log[1].toString().indexOf(argv.prefix1) == 0 : true) && (argv.prefix2 ? log[0].toString().indexOf(argv.prefix2) == 0 : true);
-    });
-  }
-
+  // Output results
   var i = 0, log;
-  if(argv.a) {
-    while(i++ < OUTPUT_LIMIT) {
+  // Ascending
+  if (argv.a) {
+    while (i++ < OUTPUT_LIMIT) {
       log = logs.shift();
-      console.log(colors.white(i) + ' - ' + colors.cyan(log[1]) + ': ' + colors.red(log[0]));
+      console.log(colors.white(i) + ' - ' + _.map(log, function (l, index) { return colors[usefulColors[index % usefulColors.length]](l) }).join(colors.white(' - ')));
     }
   }
+  // Descending
   else {
-    while(i++ < OUTPUT_LIMIT) {
+    while (i++ < OUTPUT_LIMIT) {
       log = logs.pop();
-      console.log(colors.white(i) + ' - ' + colors.cyan(log[1]) + ': ' + colors.red(log[0]));
+      console.log(colors.white(i) + ' - ' + _.map(log, function (l, index) { return colors[usefulColors[index % usefulColors.length]](l) }).join(colors.white(' - ')));
     }
   }
+}
+
+// Function to filter logs by prefixes
+function filterLogs(log) {
+  // Prefixes are strings that queried resources starts with.
+  // For example, to find URL's starting with http://example.com
+  // argument should be --prefix1=http:/example.com
+  for (var i = 1; i < argv.prefixes.length; i++) {
+    var p = argv.prefixes[i];
+    if (!p) continue;
+
+    if (String(log[i]).indexOf(p) != 0)
+      return false;
+  }
+
+  return true;
 }
